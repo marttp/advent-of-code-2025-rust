914522322d6cf11d823d9541ae301f84ce000374
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-12-02 07:53:50 +0700

    chore: clear day01 part1 and part2 solution implementations.

--- day01/src/bin/part1.rs
+++ day01/src/bin/part1.rs
@@ -8,20 +8,5 @@ fn main() {
 }
 
 fn solution(input: Vec<&str>) -> u32 {
-    let (first_list, second_list): (Vec<u32>, Vec<u32>) = input
-        .into_iter()
-        .map(|line| split_line::<u32>(line))
-        .map(|pair| (pair[0], pair[1]))
-        .unzip();
-
-    let mut cloned_first = first_list.clone();
-    cloned_first.sort();
-    let mut cloned_second = second_list.clone();
-    cloned_second.sort();
-
-    cloned_first
-        .iter()
-        .zip(cloned_second.iter())
-        .map(|(a, b)| u32::abs_diff(*a, *b))
-        .sum()
+    0
 }
